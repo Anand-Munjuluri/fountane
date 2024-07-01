eaa6e2b3879d5c75fd430a2326a8edd49db09ef8
Anand Munjuluri <100225249+Anand-Munjuluri@users.noreply.github.com>
2024-07-01 19:39:45 +0530

    alt

--- src/app/components/ui/Wobblecard2.tsx
+++ src/app/components/ui/Wobblecard2.tsx
@@ -16,7 +16,8 @@ export function WobbleCardDemo2() {
           </h2>
           <Image src="/tandm-logo.svg"
           width={100}
-          height={100}/>
+          height={100}
+          alt="logo"/>
         </div>
         <Image
           src="/Scirque.png"
